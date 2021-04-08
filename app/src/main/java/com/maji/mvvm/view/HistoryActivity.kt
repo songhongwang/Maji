3ade9ddcf09a934cbc2d1139e8bd60362ed2e470
vigorous <songhongwang@tangdou.com>
2021-04-08 11:01:19 +0800

    使用ViewBinding代替findViewById()

--- app/src/main/java/com/maji/mvvm/view/HistoryActivity.kt
+++ app/src/main/java/com/maji/mvvm/view/HistoryActivity.kt
@@ -6,19 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.maji.mvvm.R
+import com.maji.mvvm.databinding.ActivityHistoryBinding
 import com.maji.mvvm.util.RecycleViewDivider
 import com.maji.mvvm.viewmodel.SubjectViewModel
 
 class HistoryActivity : AppCompatActivity() {
     private val TAG = HistoryActivity::class.java.simpleName
     private var subjectViewModel: SubjectViewModel? = null
-    private var recyclerViewSubject: RecyclerView? = null
+    private var viewBinding: ActivityHistoryBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(viewBinding?.root)
         initUI()
 
         observeData()
@@ -26,10 +27,9 @@ class HistoryActivity : AppCompatActivity() {
 
     private fun initUI() {
         title = getString(R.string.app_history)
-        recyclerViewSubject = findViewById(R.id.rv_subject)
-        recyclerViewSubject?.addItemDecoration(RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL))
+        viewBinding?.rvSubject?.addItemDecoration(RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL))
         val lm = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        recyclerViewSubject?.layoutManager = lm
+        viewBinding?.rvSubject?.layoutManager = lm
     }
 
     private fun observeData() {
@@ -39,7 +39,7 @@ class HistoryActivity : AppCompatActivity() {
             Log.d(TAG, "history size is = ${it?.size}")
             it?.let {
                 val adapter = SubjectAdapter(it)
-                recyclerViewSubject?.adapter = adapter
+                viewBinding?.rvSubject?.adapter = adapter
                 adapter.notifyDataSetChanged()
             }
 
--- app/src/main/java/com/maji/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/maji/mvvm/view/MainActivity.kt
@@ -4,13 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.maji.mvvm.R
+import com.maji.mvvm.databinding.ActivityMainBinding
 import com.maji.mvvm.util.RecycleViewDivider
 import com.maji.mvvm.viewmodel.SubjectViewModel
 import kotlin.math.abs
@@ -19,13 +19,14 @@ import kotlin.math.abs
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
     private var subjectViewModel: SubjectViewModel? = null
-    private var recyclerViewSubject: RecyclerView? = null
-    private var btnHistory:Button? = null
     private val mScrollThreshold = 20
+
+    var viewBinding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding?.root)
         initUI()
 
         observeData()
@@ -33,13 +34,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun initUI() {
         title = getString(R.string.app_main)
-        btnHistory = findViewById(R.id.btn_history)
-        recyclerViewSubject = findViewById(R.id.rv_subject)
-        recyclerViewSubject?.addItemDecoration(RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL))
+        viewBinding?.rvSubject?.addItemDecoration(RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL))
         val lm = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        recyclerViewSubject?.layoutManager = lm
+        viewBinding?.rvSubject?.layoutManager = lm
 
-        recyclerViewSubject?.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+        viewBinding?.rvSubject?.addOnScrollListener(object :RecyclerView.OnScrollListener(){
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if(recyclerView.childCount< 10) {
@@ -57,18 +56,18 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        btnHistory?.setOnClickListener {
+        viewBinding?.btnHistory?.setOnClickListener {
             var intent = Intent(this, HistoryActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun onScrollUp() {
-        btnHistory?.visibility = View.GONE
+        viewBinding?.btnHistory?.visibility = View.GONE
     }
 
     private fun onScrollDown() {
-        btnHistory?.visibility = View.VISIBLE
+        viewBinding?.btnHistory?.visibility = View.VISIBLE
     }
 
 
@@ -79,7 +78,7 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "history size is = ${it?.size}")
             it?.let {
                 val adapter = SubjectAdapter(it)
-                recyclerViewSubject?.adapter = adapter
+                viewBinding?.rvSubject?.adapter = adapter
                 adapter.notifyDataSetChanged()
             }
 
