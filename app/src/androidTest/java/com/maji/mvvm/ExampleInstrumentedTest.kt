7b888d90de334ba6b1b9eb11b4812fe396f4bdb5
vigorous <songhongwang@tangdou.com>
2021-04-11 12:43:19 +0800

    测试用例 使用application context

--- app/src/androidTest/java/com/maji/mvvm/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/maji/mvvm/ExampleInstrumentedTest.kt
@@ -1,10 +1,14 @@
 package com.maji.mvvm
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.UiThread
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.base.MainThread
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.maji.mvvm.db.SubjectDataBase
 import com.maji.mvvm.model.Subject
 import com.maji.mvvm.net.ApiClient
 import com.maji.mvvm.net.BaseCallBack
@@ -21,18 +25,29 @@ import retrofit2.Response
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    val TAG = "log_jnit_ui"
     @Test
     @MainThread
-    fun useAppContext() {
-
+    fun testApi() {
+        val context = InstrumentationRegistry.getInstrumentation().context
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             ApiClient.service.getList().observeForever(androidx.lifecycle.Observer {
-                Log.d("log_jnit_ui", "${it?.authorizations_url}")
-
+                Log.d(TAG, "response url is ${it?.authorizations_url}")
             })
         }
 
         Thread.sleep(2000)
-
     }
+
+    @Test
+    fun testDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        val db = SubjectDataBase.getInstance(context)
+        val result = db?.subjectDao()?.getSubjectSync()
+
+        Log.d(TAG, "history size is ${result?.size}")
+    }
+
+
 }
